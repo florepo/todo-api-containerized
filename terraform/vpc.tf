d8a70e746ad0ffcd83cd6ca0aec8c43f5087533d
flow1981 <florian.stagliano@gmx.de>
2021-03-28 13:46:09 +0100

    Define vpc and sec groups

---
+++ terraform/vpc.tf
@@ -0,0 +1,46 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb-sg"
+  description = "controls access to the Application Load Balancer (ALB)"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "ecs-tasks-sg"
+  description = "allow inbound access from the ALB only"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 4000
+    to_port         = 4000
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
