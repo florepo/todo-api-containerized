8ab132c77aa3914069465c4d24bf574f86e03189
flow1981 <florian.stagliano@gmx.de>
2021-04-01 22:52:45 +0100

    Enable public ip on subnets

--- terraform/modules/todo-api/vpc.tf
+++ terraform/modules/todo-api/vpc.tf
@@ -7,11 +7,19 @@ resource "aws_subnet" "ecs_subnet_a" {
   availability_zone = "eu-west-3a"
   cidr_block = "172.31.3.0/24"
   vpc_id = aws_default_vpc.default_vpc.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ecs_subnet_3a"
+  }
 }
 
 resource "aws_subnet" "ecs_subnet_b" {
   availability_zone = "eu-west-3b"
   cidr_block = "172.31.4.0/24"
   vpc_id = aws_default_vpc.default_vpc.id
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ecs_subnet_3b"
+  }
 }
 
