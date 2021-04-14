825dcb44def7e6a4b2ee64400ac2e4fe780f4640
flow1981 <florian.stagliano@gmx.de>
2021-04-14 22:13:46 +0100

    remove config.hosts.clear

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -1,8 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << '.compute.amazonaws.com'
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
